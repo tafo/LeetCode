4336ed900fd1c022aafae9f7e33cef9e8a4334e8
Tayfun Yirdem <tayfunyirdem@gmail.com>
2021-01-25 18:08:46 +0300

    Easy - RomanToInteger - Add - Solution

---
+++ LeetCode.Problems/Easy/RomanToInteger/Solution.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Problems.Easy.RomanToInteger
+{
+    /// <summary>
+    ///
+    /// Problem >> https://leetcode.com/problems/roman-to-integer/
+    ///
+    /// Time Complexity >>
+    /// Space Complexity >>
+    /// 
+    /// </summary>
+    public class Solution
+    {
+        public int RomanToInt(string s)
+        {
+            var romans = new Dictionary<char, int>
+            {
+                {'I', 1}, 
+                {'V', 5},
+                {'X', 10},
+                {'L', 50},
+                {'C', 100},
+                {'D', 500},
+                {'M', 1000},
+            };
+            
+            var output = 0;
+            var previousValue = output = romans[s[^1]];
+            for (var i = s.Length - 2; i >= 0; i--)
+            {
+                var value = romans[s[i]];
+                if (previousValue > value)
+                {
+                    output -= value;
+                }
+                else
+                {
+                    output += value;
+                }
+
+                previousValue = value;
+            }
+            
+            return output;
+        }
+    }
+}
---
+++ LeetCode.Problems/Easy/RomanToInteger/Test.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace LeetCode.Problems.Easy.RomanToInteger
+{
+    [SuppressMessage("ReSharper", "StringLiteralTypo")]
+    public class Test
+    {
+        private readonly ITestOutputHelper _outputHelper;
+        
+        public Test(ITestOutputHelper outputHelper)
+        {
+            _outputHelper = outputHelper;
+        }
+
+        [Theory]
+        [MemberData(nameof(InputAndOutput))]
+        public void Check(string input, int expectedOutput)
+        {
+            var timer = Stopwatch.StartNew();
+            var solution = new Solution();
+            timer.Start();
+            var actualOutput = solution.RomanToInt(input);
+            timer.Stop();
+            actualOutput.Should().Be(expectedOutput);
+            _outputHelper.WriteLine($"Duration = {timer.ElapsedTicks}");
+        }
+
+        public static IEnumerable<object[]> InputAndOutput => new List<object[]>
+        {
+            new object[]
+            {
+                "III", 3
+            },
+            new object[]
+            {
+                "IV", 4
+            },
+            new object[]
+            {
+                "IX", 9
+            },
+            new object[]
+            {
+                "LVIII", 58
+            },
+            new object[]
+            {
+                "MCMXCIV", 1994
+            },
+        };
+    }
+}
