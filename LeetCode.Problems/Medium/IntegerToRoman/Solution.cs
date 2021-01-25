1ba58854773c624f0a81fe1c95ff43de2e39acaf
Tayfun Yirdem <tayfunyirdem@gmail.com>
2021-01-25 17:30:32 +0300

    Medium - IntegerToRoman - Add - Solution

---
+++ LeetCode.Problems/Medium/IntegerToRoman/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace LeetCode.Problems.Medium.IntegerToRoman
+{
+    /// <summary>
+    /// Problem >> https://leetcode.com/problems/integer-to-roman/
+    ///
+    /// Time Complexity >> O(1)
+    /// Space Complexity >> O(1)
+    /// 
+    /// </summary>
+    public class Solution
+    {
+        [SuppressMessage("ReSharper", "StringLiteralTypo")]
+        public string IntToRoman(int num)
+        {
+            var m = new[] {"", "M", "MM", "MMM"};
+            var c = new[] {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+            var x = new[] {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+            var i = new[] {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+
+            return m[num / 1000] + c[num % 1000 / 100] + x[num % 100 / 10] + i[num % 10];
+        }
+    }
+}
---
+++ LeetCode.Problems/Medium/IntegerToRoman/Test.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace LeetCode.Problems.Medium.IntegerToRoman
+{
+    [SuppressMessage("ReSharper", "StringLiteralTypo")]
+    public class Test
+    {
+        private readonly ITestOutputHelper _outputHelper;
+        
+        public Test(ITestOutputHelper outputHelper)
+        {
+            _outputHelper = outputHelper;
+        }
+
+        [Theory]
+        [MemberData(nameof(InputAndOutput))]
+        public void Check(int num, string expectedOutput)
+        {
+            var timer = Stopwatch.StartNew();
+            var solution = new Solution();
+            timer.Start();
+            var actualOutput = solution.IntToRoman(num);
+            timer.Stop();
+            actualOutput.Should().Be(expectedOutput);
+            _outputHelper.WriteLine($"Duration = {timer.ElapsedTicks}");
+        }
+
+        public static IEnumerable<object[]> InputAndOutput => new List<object[]>
+        {
+            new object[]
+            {
+                3, "III"
+            },
+            new object[]
+            {
+                4, "IV"
+            },
+            new object[]
+            {
+                9, "IX"
+            },
+            new object[]
+            {
+                58, "LVIII"
+            },
+            new object[]
+            {
+                1994, "MCMXCIV"
+            },
+        };
+    }
+}
