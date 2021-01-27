7b301e7544f2035a1c7b29628b26943d588eaa52
Tafo <tayfunyirdem@gmail.com>
2021-01-27 20:09:42 +0300

    Medium - ThreeSumClosest - Add - Solution

---
+++ LeetCode.Problems/Medium/ThreeSumClosest/Solution.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LeetCode.Problems.Medium.ThreeSumClosest
+{
+    public class Solution
+    {
+        public int ThreeSumClosest(int[] nums, int target)
+        {
+            Array.Sort(nums);
+            var result = nums[0] + nums[1] + nums[2];
+            if (target <= result) return result;
+            for (var a = 0; a < nums.Length - 2; a++)
+            {
+                var c = nums.Length - 1;
+                for (var b = a + 1; b < nums.Length - 1; b++)
+                {
+                    for (; b < c; c--)
+                    {
+                        var sum = nums[a] + nums[b] + nums[c];
+
+                        if (Math.Abs(sum - target) < Math.Abs(result - target)) result = sum;
+                        if (sum < target) break;
+                        if (sum == target) return sum;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCode.Problems/Medium/ThreeSumClosest/Test.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace LeetCode.Problems.Medium.ThreeSumClosest
+{
+    public class Test
+    {
+        private readonly ITestOutputHelper _outputHelper;
+
+        public Test(ITestOutputHelper outputHelper)
+        {
+            _outputHelper = outputHelper;
+        }
+
+        [Theory]
+        [MemberData(nameof(InputAndOutput))]
+        public void Check(int[] numbers, int target, int expectedOutput)
+        {
+            var timer = Stopwatch.StartNew();
+            var solution = new Solution();
+            timer.Start();
+            var actualOutput = solution.ThreeSumClosest(numbers, target);
+            timer.Stop();
+            actualOutput.Should().Be(expectedOutput);
+            _outputHelper.WriteLine($"Duration = {timer.ElapsedTicks}");
+        }
+
+        public static IEnumerable<object[]> InputAndOutput => new List<object[]>
+        {
+            new object[]
+            {
+                new[] {-1, 2, 1, -4}, 1, 2
+            },
+        };
+    }
+}
