bc00dc55a15bdad5122479296be55d2b89b37ba8
Tafo <tayfunyirdem@gmail.com>
2021-01-27 13:17:37 +0300

    Easy - LongestCommonPrefix - Add - Solution - LINQ

---
+++ LeetCode.Problems/Easy/LongestCommonPrefix/Solution.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+
+namespace LeetCode.Problems.Easy.LongestCommonPrefix
+{
+    /// <summary>
+    /// Problem: https://leetcode.com/problems/longest-common-prefix/
+    ///
+    /// ToDo: Check Time Complexity
+    /// ToDo: Check Space Complexity
+    /// </summary>
+    public class Solution
+    {
+        public string LongestCommonPrefix(string[] strs)
+        {
+            return strs
+                .DefaultIfEmpty("")
+                .Aggregate((x, y) => x == "" ? "" : string.Concat(x.TakeWhile((c, i) => i < y.Length && c == y[i])));
+        }
+    }
+}
---
+++ LeetCode.Problems/Easy/LongestCommonPrefix/Test.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace LeetCode.Problems.Easy.LongestCommonPrefix
+{
+    public class Test
+    {
+        private readonly ITestOutputHelper _outputHelper;
+        
+        public Test(ITestOutputHelper outputHelper)
+        {
+            _outputHelper = outputHelper;
+        }
+
+        [Theory]
+        [MemberData(nameof(InputAndOutput))]
+        public void Check(string[] stringList, string expectedOutput)
+        {
+            var timer = Stopwatch.StartNew();
+            var solution = new Solution();
+            timer.Start();
+            var actualOutput = solution.LongestCommonPrefix(stringList);
+            timer.Stop();
+            actualOutput.Should().Be(expectedOutput);
+            _outputHelper.WriteLine($"Duration = {timer.ElapsedTicks}");
+        }
+
+        public static IEnumerable<object[]> InputAndOutput => new List<object[]>
+        {
+            new object[]
+            {
+                new[] {"flower","flow","flight"}, "fl"
+            },
+            new object[]
+            {
+                new[] {"dog","racecar","car"}, string.Empty
+            },
+        };
+
+    }
+}
