b74b7704814eada493febe447b01a947aa8ffec8
Tafo <tayfunyirdem@gmail.com>
2021-01-29 17:59:17 +0300

    Medium - ValidParantheses - Add - Solution

---
+++ LeetCode.Problems/Easy/ValidParantheses/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems.Easy.ValidParantheses
+{
+    public class Solution
+    {
+        public bool IsValid(string s)
+        {
+            var charStack = new Stack<char>();
+            foreach (var c in s)
+            {
+                switch (c)
+                {
+                    case '(':
+                        charStack.Push(')');
+                        break;
+                    case '[':
+                        charStack.Push(']');
+                        break;
+                    case '{':
+                        charStack.Push('}');
+                        break;
+                    default:
+                    {
+                        if (charStack.Count == 0 || charStack.Pop() != c) return false;
+                        break;
+                    }
+                }
+            }
+
+            return charStack.Count == 0;
+        }
+    }
+}
---
+++ LeetCode.Problems/Easy/ValidParantheses/Test.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace LeetCode.Problems.Easy.ValidParantheses
+{
+    public class Test
+    {
+        private readonly ITestOutputHelper _outputHelper;
+
+        public Test(ITestOutputHelper outputHelper)
+        {
+            _outputHelper = outputHelper;
+        }
+
+        [Theory]
+        [MemberData(nameof(InputAndOutput))]
+        public void Check(string input, bool expectedOutput)
+        {
+            var timer = Stopwatch.StartNew();
+            var solution = new Solution();
+            timer.Start();
+            var actualOutput = solution.IsValid(input);
+            timer.Stop();
+            actualOutput.Should().Be(expectedOutput);
+            _outputHelper.WriteLine($"Duration = {timer.ElapsedTicks}");
+        }
+
+        public static IEnumerable<object[]> InputAndOutput => new List<object[]>
+        {
+            new object[]
+            {
+                "()", true
+            },
+            new object[]
+            {
+                "()[]{}", true
+            },
+            new object[]
+            {
+                "(]", false
+            },
+            new object[]
+            {
+                "([)]", false
+            },
+            new object[]
+            {
+                "{[]}", true
+            },
+            new object[]
+            {
+                "{()[()]}", true
+            },
+            new object[]
+            {
+                "({[)", false
+            },
+        };
+    }
+}
