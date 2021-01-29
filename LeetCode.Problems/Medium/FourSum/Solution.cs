d1472d51444ce144f81ddd3b40ad8824e8c894c6
Tafo <tayfunyirdem@gmail.com>
2021-01-29 14:49:21 +0300

    Medium - FourSum - Add - Solution

---
+++ LeetCode.Problems/Medium/FourSum/Solution.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Problems.Medium.FourSum
+{
+    /// <summary>
+    /// Problem: https://leetcode.com/problems/4sum/
+    /// </summary>
+    public class Solution
+    {
+        public IList<IList<int>> FourSum(int[] nums, int target)
+        {
+            var result = new List<IList<int>>();
+            Array.Sort(nums);
+            for (var a = 0; a < nums.Length - 3; a++)
+            {
+                if (a > 0 && nums[a] == nums[a - 1]) continue;
+                for (var b = a + 1; b < nums.Length - 2; b++)
+                {
+                    if (b > a + 1 && nums[b] == nums[b - 1]) continue;
+                    var right = nums.Length - 1;
+                    for (var c = b + 1; c < right; c++)
+                    {
+                        if (nums[a] + nums[b] + nums[c] + nums[c + 1] > target) break;
+                        if (c > b + 1 && nums[c] == nums[c - 1]) continue;
+                        var left = c + 1;
+                        while (left <= right)
+                        {
+                            var d = (left + right) >> 1;
+                            if (nums[a] + nums[b] + nums[c] + nums[d] < target)
+                            {
+                                left = d + 1;
+                            }
+                            else if (nums[a] + nums[b] + nums[c] + nums[d] > target)
+                            {
+                                right = d - 1;
+                            }
+                            else
+                            {
+                                result.Add(new List<int> { nums[a], nums[b], nums[c], nums[d] });
+                                right = d - 1;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCode.Problems/Medium/FourSum/Test.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using FluentAssertions;
+using LeetCode.Problems.Common;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace LeetCode.Problems.Medium.FourSum
+{
+    public class Test
+    {
+        private readonly ITestOutputHelper _outputHelper;
+
+        public Test(ITestOutputHelper outputHelper)
+        {
+            _outputHelper = outputHelper;
+        }
+
+        [Theory]
+        [MemberData(nameof(InputAndOutput))]
+        public void Check(int[] numbers, int target, IList<IList<int>> expectedOutput)
+        {
+            var timer = Stopwatch.StartNew();
+            var solution = new Solution();
+            timer.Start();
+            var actualOutput = solution.FourSum(numbers, target);
+            timer.Stop();
+            actualOutput.Should().BeEqualTo(expectedOutput);
+            _outputHelper.WriteLine($"Duration = {timer.ElapsedTicks}");
+        }
+
+        public static IEnumerable<object[]> InputAndOutput => new List<object[]>
+        {
+            new object[]
+            {
+                new[] {1, 0, -1, 0, -2, 2},
+                0,
+                new List<IList<int>>
+                {
+                    new List<int>
+                    {
+                        -2, -1, 1, 2
+                    },
+                    new List<int>
+                    {
+                        -2, 0, 0, 2
+                    },
+                    new List<int>
+                    {
+                        -1, 0, 0, 1
+                    }
+                }
+            },
+            new object[]
+            {
+                new int[] { },
+                0,
+                new List<IList<int>>()
+            },
+        };
+    }
+}
