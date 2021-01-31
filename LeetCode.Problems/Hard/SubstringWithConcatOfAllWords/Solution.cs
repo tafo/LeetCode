4b5695bd858398bca3d4c6e200b342e20f96c4b7
Tafo <tayfunyirdem@gmail.com>
2021-01-31 20:11:52 +0300

    Hard - SubstringWithConcatOfAllWords - Add - Solution

---
+++ LeetCode.Problems/Hard/SubstringWithConcatOfAllWords/Solution.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems.Hard.SubstringWithConcatOfAllWords
+{
+    public class Solution
+    {
+        public IList<int> FindSubstring(string s, string[] words)
+        {
+            var output = new List<int>();
+            var wordLength = words[0].Length;
+            var windowLength = wordLength * words.Length;
+            var wordCounterMap = new Dictionary<string, int>();
+            var wordFoundMap = new Dictionary<string, int>();
+            foreach (var word in words)
+            {
+                if (wordCounterMap.ContainsKey(word)) wordCounterMap[word]++;
+                else wordCounterMap[word] = 1;
+            }
+
+            for (var i = 0; i <= s.Length - windowLength; i++)
+            {
+                wordFoundMap.Clear();
+                CheckWindow(i);
+            }
+
+            return output;
+
+            void CheckWindow(int i)
+            {
+                for (var j = 0; j < words.Length; j++)
+                {
+                    var start = i + j * wordLength;
+                    var word = s.Substring(start, wordLength);
+
+                    if (!wordCounterMap.ContainsKey(word)) return;
+
+                    if (wordFoundMap.ContainsKey(word)) wordFoundMap[word]++;
+                    else wordFoundMap[word] = 1;
+
+                    if (wordFoundMap[word] > wordCounterMap[word]) return;
+                }
+                output.Add(i);
+            }
+        }
+    }
+}
---
+++ LeetCode.Problems/Hard/SubstringWithConcatOfAllWords/Test.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace LeetCode.Problems.Hard.SubstringWithConcatOfAllWords
+{
+    public class Test
+    {
+        private readonly ITestOutputHelper _outputHelper;
+
+        public Test(ITestOutputHelper outputHelper)
+        {
+            _outputHelper = outputHelper;
+        }
+
+        [Theory]
+        [MemberData(nameof(InputAndOutput))]
+        public void Check(string s, string[] words, IList<int> expectedOutput)
+        {
+            var timer = Stopwatch.StartNew();
+            var solution = new Solution();
+            timer.Start();
+            var actualOutput = solution.FindSubstring(s, words);
+            timer.Stop();
+            actualOutput.Should().BeEquivalentTo(expectedOutput);
+            _outputHelper.WriteLine($"Duration = {timer.ElapsedTicks}");
+        }
+
+        public static IEnumerable<object[]> InputAndOutput => new List<object[]>
+        {
+            new object[]
+            {
+                "barfoothefoobarman",
+                new List<string> { "foo", "bar" },
+                new List<int> {0, 9}
+            },
+            new object[]
+            {
+                "barfoofoobarthefoobarman",
+                new List<string> { "bar", "foo", "the" },
+                new List<int> {6, 9, 12}
+            },
+            new object[]
+            {
+                "wordgoodgoodgoodbestword",
+                new List<string> { "word","good","best","good" },
+                new List<int> {8}
+            },
+            new object[]
+            {
+                "wordgoodthegoodgoodbestword",
+                new List<string> { "word","good","best","good" },
+                new List<int> {11}
+            },
+            new object[]
+            {
+                "lingmindraboofooowingdingbarrwingmonkeypoundcake",
+                new List<string> { "fooo","barr","wing","ding","wing" },
+                new List<int> {13}
+            },
+            new object[]
+            {
+                "ababababab",
+                new List<string> { "ababa","babab" },
+                new List<int> {0}
+            },
+            new object[]
+            {
+                "ababaab",
+                new List<string> { "ab","ba", "ba" },
+                new List<int> {1}
+            },
+        };
+    }
+}
