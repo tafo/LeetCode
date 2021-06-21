00815fe5477f57f248f9c9c80cd71d6c47de2805
Tayfun Yirdem <tayfunyirdem@gmail.com>
2021-06-21 10:22:25 +0300

    Easy - RemoveDuplicates - Add - Solution

---
+++ LeetCode.Problems/Easy/RemoveDuplicates/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode.Problems.Easy.RemoveDuplicates
+{
+    public class Solution
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            if (nums.Length == 0) return 0;
+            if (nums.Length == 1) return 1;
+
+            var index = 0;
+            for (var i = 1; i < nums.Length; i++)
+            {
+                if (nums[index] == nums[i])
+                {
+                    continue;
+                }
+
+                index++;
+                nums[index] = nums[i];
+            }
+
+            return index + 1;
+        }
+    }
+}
---
+++ LeetCode.Problems/Easy/RemoveDuplicates/Test.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace LeetCode.Problems.Easy.RemoveDuplicates
+{
+    public class Test
+    {
+        private readonly ITestOutputHelper _outputHelper;
+
+        public Test(ITestOutputHelper outputHelper)
+        {
+            _outputHelper = outputHelper;
+        }
+
+        public static IEnumerable<object[]> InputAndOutput => new List<object[]>
+        {
+            new object[]
+            {
+                new[]{1,1,2}, new[]{1,2,2}, 2
+            },
+            new object[]
+            {
+                new[]{0,0,1,1,1,2,2,3,3,4}, new[]{0,1,2,3,4,2,2,3,3,4}, 5
+            },
+        };
+
+        [Theory]
+        [MemberData(nameof(InputAndOutput))]
+        public void Check(int[] numbers, int[] expectedNumbers, int k)
+        {
+            var solution = new Solution();
+            var timer = Stopwatch.StartNew();
+            var actualOutput = solution.RemoveDuplicates(numbers);
+            timer.Stop();
+
+            Assert.Equal(k, actualOutput);
+
+            for (var i = 0; i < actualOutput; i++)
+            {
+                Assert.Equal(expectedNumbers[i], numbers[i]);
+            }
+
+            _outputHelper.WriteLine($"Duration = {timer.ElapsedTicks}");
+        }
+    }
+}
